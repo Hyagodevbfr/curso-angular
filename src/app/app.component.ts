10886d838a5879e7959ab8297f0c362e4b07b071
hyagodevbfr <hyago.sbrito@gmail.com>
2025-03-13 11:58:43 -0300

    feat: practices data sharing via input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  userName = 'Daniel';
+  person = {
+    age: 25,
+    nationalId: '123.456.789-00',
+  };
   title = 'curso-angular';
 }
--- src/app/components/parent-data/parent-data.component.ts
+++ src/app/components/parent-data/parent-data.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-parent-data',
   templateUrl: './parent-data.component.html',
-  styleUrls: ['./parent-data.component.scss']
+  styleUrls: ['./parent-data.component.scss'],
 })
-export class ParentDataComponent {
+export class ParentDataComponent implements OnInit {
+  @Input() name: string = '';
+  @Input() personInfo!: { age: number; nationalId: string };
 
+  constructor() {}
+  ngOnInit(): void {}
 }
