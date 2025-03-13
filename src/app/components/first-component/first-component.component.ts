7462ac80e77abf6ce4cedeb544185c98edfdb3cf
hyagodevbfr <hyago.sbrito@gmail.com>
2025-03-13 10:32:47 -0300

    feat: add data interpolation in the first component

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
-  styleUrls: ['./first-component.component.scss']
+  styleUrls: ['./first-component.component.scss'],
 })
-export class FirstComponentComponent {
+export class FirstComponentComponent implements OnInit {
+  name: string = 'Hyago';
+  age: number = 25;
+  job = 'Desenvolvedor';
+  hobbie = ['Ler', 'Estudar', 'Correr'];
+  car = {
+    brand: 'Chevrolet',
+    model: 'Astra',
+    year: 2011,
+  };
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
