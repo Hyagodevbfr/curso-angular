190b28c8a060ef2dedbd403c0b120a1165d43981
hyagodevbfr <hyago.sbrito@gmail.com>
2025-03-13 10:14:55 -0300

    feat: create component alone

---
+++ src/app/components/own-component/own-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-own-component',
+  templateUrl: './own-component.component.html',
+  styleUrls: ['./own-component.component.scss']
+})
+export class OwnComponentComponent {
+
+}
